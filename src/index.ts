da8e118f7124ca7e684b8e31d590476da8594649
devzotic <devzotic@gmail.com>
2023-12-17 13:01:23 +0700

    add example for using findLongestPrefix function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+import { findLongestPrefix } from "./findLongestPrefix"
 
+const exam1 = ["flower", "flow", "flight"]
+const exam2 = ["dog", "racecar", "car"]
+
+const result1 = findLongestPrefix(exam1)
+const result2 = findLongestPrefix(exam2)
+
+console.log(result1)
+console.log(result2)
